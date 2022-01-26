77ade243229cfb31046957d741b95332fe85c0e8
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-26 14:58:11 -0500

    add navbar titling

--- expense/src/App.js
+++ expense/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Nav, Navbar, Container, Link } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Contacts</Nav.Link>
+              <Nav.Link href="#pricing">Expense Tracker</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
     </div>
   );
 }
