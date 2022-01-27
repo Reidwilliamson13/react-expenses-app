24e59c82874faf35992726297dedce409c2c42a3
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-27 11:03:39 -0500

    add new print component

--- expense app/src/App.js
+++ expense app/src/App.js
@@ -5,32 +5,45 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home"
 import Contacts from "./components/Contacts";
 import ExpenseTracker from "./components/ExpenseTracker";
+import Print from "./components/Print";
 
 
 function App() {
   return (
-    <div className="App">
-      <>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to='/'>Home</Nav.Link>
-              <Nav.Link as={Link} to='/contacts'>Contacts</Nav.Link>
-              <Nav.Link as={Link} to='/expensetracker'>Expense Tracker</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
+    <BrowserRouter>
+      <div className="App">
+        <>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/contacts">
+                  Contacts
+                </Nav.Link>
+                <Nav.Link as={Link} to="/expensetracker">
+                  Expense Tracker
+                </Nav.Link>
+                <Nav.Link as={Link} to="/print">
+                  Print
+                </Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </>
 
-<div>
-    <Routes>
-      <Route path="./" element={<Home />}/>
-      <Route path="./Contacts" element={<Contacts />}/>
-      <Route path="./ExpenseTracker" element={<ExpenseTracker />}/>
-    </Routes>
-</div>
-    </div>
+        <div>
+          <Routes>
+            <Route path="./" element={<Home />} />
+            <Route path="./contacts" element={<Contacts />} />
+            <Route path="./expensetracker" element={<ExpenseTracker />} />
+            <Route path="./print" element={<Print />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
